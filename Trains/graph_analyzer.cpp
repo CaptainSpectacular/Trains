9904b9f93656858e340582c5c30c9f22c41a4b41
cpollock <pollock.clee@gmail.com>
2018-10-14 13:25:05 -0600

    Introducing the GraphAnalyzer.

---
+++ Trains/graph_analyzer.cpp
@@ -0,0 +1,7 @@
+#include "graph_analyzer.h"
+#include "graph.h"
+
+GraphAnalyzer::GraphAnalyzer(Graph* graph)
+{
+	this->graph = graph;
+}
---
+++ Trains/graph_analyzer.h
@@ -0,0 +1,11 @@
+#pragma once
+class Graph;
+
+class GraphAnalyzer
+{
+public:
+	GraphAnalyzer(Graph* graph);
+
+private:
+	Graph* graph;
+};
